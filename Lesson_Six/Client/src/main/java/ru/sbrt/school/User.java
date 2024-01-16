57f27869485acdff26743712f04193783794b704
Shipulina <61729850+preppy5116@users.noreply.github.com>
2024-01-16 15:39:16 +0300

    Add files via upload

---
+++ Lesson_Six/Client/src/main/java/ru/sbrt/school/User.java
@@ -0,0 +1,13 @@
+package ru.sbrt.school;
+
+public class User {
+    int userID;
+
+    public User(int userID) {
+        this.userID = userID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+}
---
+++ Lesson_Six/Message/src/main/java/ru/sbrt/school/MessageCreator.java
@@ -0,0 +1,13 @@
+package ru.sbrt.school;
+
+public class MessageCreator {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
